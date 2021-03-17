956abbbe64fdea15d60556d8d1eda03a087756c8
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2021-03-17 12:26:41 -0300

    Cria component reaproveitável ProductCard

---
+++ components/ProductCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <nuxt-link :to="path" class="grid">
+    <article class="bg-white border border-gray-300 rounded shadow-lg">
+      <img
+        v-if="image"
+        :src="image"
+        :alt="title"
+        class="block rounded-t"
+      />
+
+      <div class="p-3">
+        <h2 class="text-lg font-bold text-blue-900">{{ title }}</h2>
+        <p class="text-gray-700">{{ description }}</p>
+        <strong class="mt-2 text-2xl text-green-700">R$ {{ price }}</strong>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+
+  props: {
+    path: {
+      type: String,
+      default: ''
+    },
+
+    title: {
+      type: String,
+      default: ''
+    },
+
+    description: {
+      type: String,
+      default: ''
+    },
+
+    price: {
+      type: String,
+      default: ''
+    },
+
+    image: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
